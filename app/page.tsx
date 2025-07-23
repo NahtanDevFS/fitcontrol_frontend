7a07a394e8d791a2af818cd20828612bcc5750cb
NahtanDevFS <jonathan04franco@gmail.com>
2025-07-23 00:37:22 -0600

    creación del frontend

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import "./page.css";
+
+export default function Home() {
+  return (
+    <div className="page">
+      <main className="main"></main>
+      <footer className="footer"></footer>
+    </div>
+  );
+}
