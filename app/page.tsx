cabb1792e760b68dcd1e9b9f83c34efc3b4a97bb
NahtanDevFS <jonathan04franco@gmail.com>
2025-08-21 22:56:27 -0600

    feat: cambios menores

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import "./page.css";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Verificar si estamos en el cliente (navegador)
+    if (typeof window !== "undefined") {
+      const userFitControl = localStorage.getItem("userFitControl");
+
+      if (!userFitControl) {
+        // Si no existe userFitControl, redirigir a /login
+        router.push("/login");
+      } else {
+        // Si existe userFitControl, redirigir a /dashboard
+        router.push("/dashboard");
+      }
+    }
+  }, [router]);
+
   return (
     <div className="page">
-      <main className="main">hola mundo</main>
+      <main className="main">Cargando...</main>
       <footer className="footer"></footer>
     </div>
   );
