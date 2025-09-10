53c3e35f859b5bebd5c02d8a8f15c7f2c9e383d2
NahtanDevFS <jonathan04franco@gmail.com>
2025-09-10 01:54:37 -0600

    fix: cambios menores

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { supabase } from "@/lib/supabase";
+//import { supabase } from "@/lib/supabase";
 import Link from "next/link";
 import {
   CumplimientoDietaDia,
